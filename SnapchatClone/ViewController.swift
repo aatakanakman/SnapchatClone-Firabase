9a888e560bdb031dbcbcbb06c693db1835990b17
aatakanakman <48566734+aatakanakman@users.noreply.github.com>
2020-10-12 22:36:55 +0300

    Initial Commit

---
+++ SnapchatClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Ali Atakan AKMAN on 12.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
